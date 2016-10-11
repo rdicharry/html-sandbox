09ee541fd30a22b84e4c753d0f74d3174542c883
Rebecca Dicharry <rdicharry@gmail.com>
2016-10-11 13:38:26 -0600

    Added section one.

--- public_html/index.php
+++ public_html/index.php
@@ -5,6 +5,32 @@
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
-		<h1>Hello World!</h1>
+		<!-- Header information -->
+		<header>
+			<h1>Hi! This is my page.</h1>
+		</header>
+
+		<!-- Content goes here -->
+		<main>
+			<!-- begin section 1 -->
+			<section>
+				<p>
+					Pea horseradish azuki bean lettuce avocado asparagus okra. Kohlrabi radish okra azuki bean corn fava bean mustard tigernut jÃ­cama green bean celtuce collard greens avocado quandong fennel gumbo black-eyed pea. Grape silver beet watercress potato tigernut corn groundnut. Chickweed okra pea winter purslane coriander yarrow sweet pepper radish garlic brussels sprout groundnut summer purslane earthnut pea tomato spring onion azuki bean gourd. Gumbo kakadu plum komatsuna black-eyed pea green bean zucchini gourd winter purslane silver beet rock melon radish asparagus spinach.
+				</p>
+				<img src="img/sandias.jpg" alt="The Sandia Mountains">
+			</section>
+
+			<!-- begin section 2 -->
+			<section>
+
+			</section>
+
+			<!-- begin section 3 -->
+			<section></section>
+
+		</main>
+
+
+
 	</body>
 </html>
