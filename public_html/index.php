23b1fc2d6da1e04592796e4c881944364306e0a8
Rebecca Dicharry <rdicharry@gmail.com>
2016-10-11 13:56:36 -0600

    Added Sections Two and Three

--- public_html/index.php
+++ public_html/index.php
@@ -17,16 +17,41 @@
 				<p>
 					Pea horseradish azuki bean lettuce avocado asparagus okra. Kohlrabi radish okra azuki bean corn fava bean mustard tigernut jÃ­cama green bean celtuce collard greens avocado quandong fennel gumbo black-eyed pea. Grape silver beet watercress potato tigernut corn groundnut. Chickweed okra pea winter purslane coriander yarrow sweet pepper radish garlic brussels sprout groundnut summer purslane earthnut pea tomato spring onion azuki bean gourd. Gumbo kakadu plum komatsuna black-eyed pea green bean zucchini gourd winter purslane silver beet rock melon radish asparagus spinach.
 				</p>
-				<img src="img/sandias.jpg" alt="The Sandia Mountains">
+				<img src="img/molecule.png" alt="Caffeine and cAMP">
 			</section>
 
 			<!-- begin section 2 -->
 			<section>
+				<h2>My Favorite Things</h2>
+					<ul>
+						<li>Puppies!</li>
+						<li>Sandwiches</li>
+						<li>Rocks</li>
+					</ul>
 
 			</section>
 
 			<!-- begin section 3 -->
-			<section></section>
+			<section>
+				<table>
+					<tr>
+						<th>Column One</th>
+						<th>Column Two</th>
+					</tr>
+					<tr>
+						<td>Cat</td>
+						<td>Dog</td>
+					</tr>
+					<tr>
+						<td>Car</td>
+						<td>Bike</td>
+					</tr>
+					<tr>
+						<td>Star Wars</td>
+						<td>Wine</td>
+					</tr>
+				</table>
+			</section>
 
 		</main>
 
